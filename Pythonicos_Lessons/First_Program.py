365ce364e51a0a2e2b98958795841f62e23ee3a4
Nicolas Silva <nicolas.silva@netsecurity.com.br>
2020-09-13 20:38:51 -0300

    Pythonicos, Lesson 8.

--- Pythonicos_Lessons/First_Program.py
+++ Pythonicos_Lessons/First_Program.py
@@ -1,15 +1,21 @@
 ### Using raw_input for catch user's input.###
-#ip = raw_input('Digite um endereco IP: ')
-#print 'O endereco  '+ ip + ' e um endereco valido'
-
+'''
+ip = raw_input('Digite um endereco IP: ')
+print 'O endereco  '+ ip + ' e um endereco valido'
+'''
 ### Printing a variable. In this case,ip. %s Used to show a value without putting it in the text.###
-#print 'O endereco %s e um endereco valido' % ip
-
+'''
+print 'O endereco %s e um endereco valido' % ip
+'''
 ###Counting type int, using %d (for integer) and Len() on counting.###
-#print 'O endereco %s tem um tamanho de %d caracteres' %(ip,len(ip))
-
+'''
+print 'O endereco %s tem um tamanho de %d caracteres' %(ip,len(ip))
+'''
 ###Convert counting in type string. Use the variable str() for conversion.###
-#print 'O endereco %s tem um tamanho de %s caracteres' %(ip,str(len(ip)))
-
+'''
+print 'O endereco %s tem um tamanho de %s caracteres' %(ip,str(len(ip)))
+'''
 ###Counting float numbers, using %f. To show 2 numbers after comma, use .n before the f (n = number)###
-#print 'Numero quebrado %.2f ' %1.345678
+'''
+print 'Numero quebrado %.2f ' %1.345678
+'''
---
+++ Pythonicos_Lessons/While.py
@@ -0,0 +1,29 @@
+### While is just a looping ###
+'''
+contador = 0
+while contador <= 255:
+print '192.168.0.' + (str(contador))
+contador = contador + 1
+'''
+
+
+### Using break to stop looping ###
+'''
+while True:
+    user = raw_input('Enter your username: ')
+    print ('Invalid username')
+    if user == 'larinha':
+        print "You're logged successfully!"
+        break
+'''
+### Continue While ###
+
+contador = 0
+while contador <= 1024:
+    print contador
+    conecta
+    recebe_resposta
+    if recebe_resposta = 'error':
+        continue
+
+    contador = contador + 1
