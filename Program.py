17cb37a721d2a7b24117b184317cb819da151c2f
Nicolas Silva <nicolas.silva@netsecurity.com.br>
2020-09-13 18:39:20 -0300

    Pythonicos, Lesson 5.1

--- Program.py
+++ Program.py
@@ -1,9 +1,8 @@
-# Using raw_input for catch.
+# Using raw_input for catch user's input.
 #ip = raw_input('Digite um endereco IP: ')
 #print 'O endereco  '+ ip + ' e um endereco valido'
 
-# Printing a variable. In this case,ip. %s Used to show a value without putting it
-#in the text.
+# Printing a variable. In this case,ip. %s Used to show a value without putting it in the text.
 #print 'O endereco %s e um endereco valido' % ip
 
 #Counting type int, using %d (for integer) and Len() on counting.
@@ -12,6 +11,5 @@
 #Convert counting in type string. Use the variable str() for conversion.
 #print 'O endereco %s tem um tamanho de %s caracteres' %(ip,str(len(ip)))
 
-#Counting float numbers. Use %f.
-# To show 2 numbers after comma, use .n before the f (n = number)
+#Counting float numbers, using %f. To show 2 numbers after comma, use .n before the f (n = number)
 #print 'Numero quebrado %.2f ' %1.345678
